f2d813e56ecd2527ce45205ebd4baa6e61acdec3
ssayols <sergisayolspuig@gmail.com>
2021-06-08 16:19:51 +0200

    rebuild

--- scripts/build.R
+++ scripts/build.R
@@ -17,14 +17,18 @@ CORES = 8
 ## prepare build environment
 unlink(c("staging", "docs"), recursive=TRUE)
 dir.create("staging")
+dir.create("staging/proteins")  # where the rendered proteins go
 file.copy(c("_site.yml", "_footer.html", "about.Rmd", "datasets.Rmd", "downloads.Rmd", "index.Rmd", "images"),
           "staging", recursive=TRUE)
+
+##  1-statically build the root of the website
+rmarkdown::render_site("staging")#, quiet=TRUE)
 
 ##  1-for each protein listed in data/uniprot.tab
 proteins <- head(read.delim("data/uniprot.tab"))
 template <- paste(readLines("templates/protein.Rmd"), collapse="\n")
 
-##   1.1-fills in the template/protein.Rmd as <<uniprot_id>>.Rmd
+##   1.1-fills in the template/protein.Rmd as <<uniprot_id>>.Rmd and renders it to html
 invisible({
   mclapply(seq_len(nrow(proteins)), function(i) {
     # the metadata section
@@ -42,18 +46,25 @@ invisible({
     x       <- gsub("<<TEST.PLOT>>", html, x)
     
     # save file
-    writeLines(x, file.path("staging", paste0(proteins$Entry[i], ".Rmd")))
-  }, mc.cores=CORES)
+    rmd <- file.path("staging", paste0(proteins$Entry[i], ".Rmd"))
+    writeLines(x, rmd)
+    rmarkdown::render_site(rmd)#, quiet=TRUE)
+    file.copy(file.path("staging/_site", paste0(proteins$Entry[i], ".html")), "staging/_site/proteins")
+    file.remove(rmd)
+  }, mc.cores=1)
 })
 
-##  1-statically build the root of the website
-rmarkdown::render_site("staging")#, quiet=TRUE)
-
+## generate help like in distill:::write_search_json("staging", rmarkdown:::site_config("staging"))
+h <-  data.frame(title      =proteins$Gene.names...primary..,
+                 description=paste(proteins$Gene.names, "❯", proteins$Protein.names))
+h$path <- file.path("proteins", paste0(proteins$Entry, ".html"))
+writeLines(jsonlite::toJSON(h), "staging/_site/search.json")
+  
 ## 
 file.rename(file.path("staging", "_site"), "docs")
 unlink("staging", recursive=TRUE)
 
 ##  3-minify the search, to do it only on the description of the page
-system("sed -i '/^      \"contents\": /d;/^      \"author\": /d;/^      \"last_modified\": /d' docs/search.json")
+#system("sed -i '/^      \"contents\": /d;/^      \"author\": /d;/^      \"last_modified\": /d' docs/search.json")
 
 ##  3-`git commit -am'rebuild' && git push`, and go to http://ssayols.github.io/DDRBase2
